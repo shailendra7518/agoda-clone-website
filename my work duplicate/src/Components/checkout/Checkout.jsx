570fc667482924758b30b55728485db50f82ef8b
Your Name <shailendraftp0001@gmail.com>
2022-04-03 08:07:33 +0530

    added

--- my work duplicate/src/Components/checkout/Checkout.jsx
+++ my work duplicate/src/Components/checkout/Checkout.jsx
@@ -1,6 +1,7 @@
 import { Search } from "@material-ui/icons";
 import { useState } from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 import "./checkout.css";
 import { Customer } from "./customer/Customer";
@@ -8,8 +9,15 @@ import { Payment } from "./payment/Payment";
 
 
 export const Checkout = () => {
+    const navigate=useNavigate()
 let {HotelAddress,HotelName,Numberofroom,Price,Roomimage,Roomtype,Search}=JSON.parse(localStorage.getItem("ReservedRoom"))
     let information_in_store = useSelector((state) => (state.information.information));
+   
+      let auth=JSON.parse(localStorage.getItem("user")) || JSON.parse(localStorage.getItem("userDetails"))
+      if(!auth){
+        alert("please login first")
+           navigate("/login")
+      }
 
     return (
         <div className="checkout__container">
